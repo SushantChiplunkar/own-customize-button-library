f20cd174068b41ffc5cf7bbafa3a5edb7b43783b
sushant chiplunkar <sushantchiplunkar.developer@gmail.com>
2024-03-09 15:37:25 +0530

    changes for add License

--- lib/own_customize_button.dart
+++ lib/own_customize_button.dart
@@ -1,7 +1,29 @@
 library own_customize_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class OwnCustomizeButton extends StatelessWidget {
+  final onPressed;
+  final Widget child;
+  final style;
+  const OwnCustomizeButton(
+      {super.key, required this.child, required this.onPressed, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: style ??
+          TextButton.styleFrom(
+            padding: const EdgeInsets.all(16.0),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue,
+            elevation: 9.0,
+            textStyle: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+      child: child,
+    );
+  }
 }
